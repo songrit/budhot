76b694acc3a6b2fdc8363c1f96f38b56d4302418
Songrit Leemakdej <songrit@gmail.com>
2012-09-07 14:47:49 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Budhot::Application.config.secret_token = '462832444de30f8b50731a74888089d9b62e2d1e4a1d7d55aad135d5eb3fd8b0afc40f677f67a2b73281b883d4b52abf4eb1496ab6adad3210958651dfd449b6'

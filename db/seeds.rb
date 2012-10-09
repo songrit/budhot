719610a78bda31745ce6bf2d3fb0dad1180265b7
songrit <songrit@gmail.com>
2012-10-09 14:36:05 +0700

    fix menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+identity= Identity.create :name => "admin", :email => "admin@test.com", :password => "secret",
+  :password_confirmation => "secret"
+User.create :provider => "identity", :uid => identity.id.to_s, :name => identity.name,
+  :email => identity.email, :role => "M,A,D"
